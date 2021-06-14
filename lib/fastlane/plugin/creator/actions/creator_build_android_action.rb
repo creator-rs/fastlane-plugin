2e3f90708c44c6ee27d4f5c6ec4f849676a47fc6
David Ackerman <enfipy@gmail.com>
2021-06-14 04:31:07 +0300

    Add creator build android

--- lib/fastlane/plugin/creator/actions/creator_build_android_action.rb
+++ lib/fastlane/plugin/creator/actions/creator_build_android_action.rb
@@ -3,9 +3,9 @@ require_relative '../helper/creator_helper'
 
 module Fastlane
   module Actions
-    class CreatorBuildIosAction < Action
+    class CreatorBuildAndroidAction < Action
       def self.run(params)
-        UI.message("Building project for ios with creator")
+        UI.message("Building project for android with creator")
 
         (!params.nil? && !params[:project_path].nil?) || UI.user_error!("Mandatory parameter :project_path not specified")
 
@@ -13,9 +13,9 @@ module Fastlane
         # Params
         #
         project_path = params[:project_path]
-        profile_path = params[:profile_path]
-        team_id = params[:team_id]
-        identity = params[:identity]
+        sign_key_path = params[:sign_key_path]
+        sign_key_pass = params[:sign_key_pass]
+        build_envs = params[:build_envs]
 
         creator_bin = Helper::CreatorHelper.ensure_creator_exists
         if creator_bin != nil
@@ -24,10 +24,8 @@ module Fastlane
           UI.message("Creator does not exists!")
         end
 
-        cmd = "#{creator_bin} build apple --release --target=aarch64-apple-ios"
-        cmd += " --profile-path=#{profile_path}"
-        cmd += " --team-identifier=#{team_id}"
-        cmd += " --identity=#{identity}"
+        cmd = "#{build_envs} #{creator_bin} build android --release"
+        cmd += " --sign-key-path=#{sign_key_path} --sign-key-pass=#{sign_key_pass}"
         if system("cd #{project_path} && #{cmd}")
           UI.message("Success!")
         else
@@ -36,7 +34,7 @@ module Fastlane
       end
 
       def self.description
-        "Builds ipa file from rust project"
+        "Builds apk file from rust project"
       end
 
       def self.authors
@@ -57,25 +55,25 @@ module Fastlane
                                        env_name: 'CREATOR_PROJECT_PATH',
                                        description: 'Path to Rust project with Cargo.toml',
                                        optional: false),
-          FastlaneCore::ConfigItem.new(key: :profile_path,
-                                       env_name: 'CREATOR_PROFILE_PATH',
-                                       description: 'Apple Profile path for signing',
+          FastlaneCore::ConfigItem.new(key: :sign_key_path,
+                                       env_name: 'CREATOR_SIGN_KEY_PATH',
+                                       description: 'Path to the signing key',
                                        optional: false),
-          FastlaneCore::ConfigItem.new(key: :team_id,
-                                       env_name: 'CREATOR_TEAM_ID',
-                                       description: 'Apple Team ID for signing',
+          FastlaneCore::ConfigItem.new(key: :sign_key_pass,
+                                       env_name: 'CREATOR_SIGN_KEY_PASS',
+                                       description: 'Password for the signing key',
                                        optional: false),
-          FastlaneCore::ConfigItem.new(key: :identity,
-                                       env_name: 'CREATOR_IDENTITY',
-                                       description: 'Apple identity for signing',
-                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :build_envs,
+                                       env_name: 'CREATOR_BUILD_ENVS',
+                                       description: 'Build environment variables',
+                                       optional: true),
         ]
       end
 
       def self.is_supported?(platform)
         # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
         # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
-        [:ios].include?(platform)
+        [:android].include?(platform)
       end
     end
   end
